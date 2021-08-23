0d2f1cb0afd551d2a23980e1896b5a4f9ea351f9
Vadym Barabanov <introvert.png@gmail.com>
2021-08-23 14:38:08 +0300

    Add url checker functionality

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type urlResponse struct {
+	url    string
+	status string
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	c := make(chan urlResponse)
+	results := map[string]string{}
+	urls := []string{
+		"https://www.google.com",
+		"https://www.airbnb.com",
+		"https://www.amazon.com",
+		"https://www.reddit.com",
+		"https://soundcloud.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+	}
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		res := <-c
+		results[res.url] = res.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, ":", status)
+	}
+}
+
+func hitURL(url string, c chan<- urlResponse) {
+	res, err := http.Get(url)
+
+	if err != nil || res.StatusCode >= 400 {
+		c <- urlResponse{status: "FAILED", url: url}
+	} else {
+		c <- urlResponse{status: "OK", url: url}
+	}
 }
